18a4e2867b53d7d443941bf2cc7b8b0de0cf0507
tiroxxx <christianmarquez41@gmail.com>
2020-11-27 16:41:34 -0500

    adding styling to home page

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+var navButton = document.querySelector(".navigation-button");
+var navMenu = document.querySelector(".navigation-menu");
+var win = window;
+
+console.log(win);
+
+function openMenu(event) {
+
+    navButton.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+    event.preventDefault();
+    event.stopImmediatePropagation();
+}
+
+function closeMenu(event) {
+    console.log("here");
+    event.preventDefault()
+    if (navButton.classList.contains("active")) {
+        navButton.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
+}
+navButton.addEventListener("click", openMenu, false);
+
+win.addEventListener("click", closeMenu, false);
