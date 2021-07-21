b9aad2aecb5e4c6b8ae6019dd880803bb1762f5f
Christian Marquez <christianmarquez41@gmail.com>
2021-07-20 22:16:19 -0400

    added info to navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,25 @@
-import "./topbar.scss"
+import './topbar.scss';
+import { Person, Mail } from '@material-ui/icons';
 
 export default function Topbar() {
-    return (
-        <div className="topbar">
-            
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Christian Marquez
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+1 (352) 242-7902</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>christianmarquez41@gmail.com</span>
+          </div>
         </div>
-    )
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 }
