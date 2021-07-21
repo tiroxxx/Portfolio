12cce0613a9fcc12a37d685601124f380d8169e2
Christian Marquez <christianmarquez41@gmail.com>
2021-07-21 19:27:43 -0400

    added popup menu when hamburger button is clicked

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Topbar from './components/topbar/Topbar';
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
-import "./app.scss"
+import About from './components/about/About';
+import Menu from './components/hamburgerMenu/Menu';
+import './app.scss';
 
 function App() {
-
-  const[menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <div className="app">
-      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
+        <About />
         <Portfolio />
         <Contact />
       </div>
--- src/components/hamburgerMenu/Menu.jsx
+++ src/components/hamburgerMenu/Menu.jsx
@@ -1,11 +1,14 @@
 import "./menu.scss"
 
-export default function Menu() {
+export default function Menu({ menuOpen, setMenuOpen }) {
     return (
-        <div className="menu">
+        <div className={"menu " + (menuOpen && "active")}>
             <ul>
                 <li>
                     <a href="#intro">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
                 </li>
                 <li>
                     <a href="#portfolio">Portfolio</a>
