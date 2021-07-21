50c29006f98a1897731a715c16d3ca98ab72b917
Christian Marquez <christianmarquez41@gmail.com>
2021-07-21 18:50:55 -0400

    adding hamburger menu icon

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -18,7 +18,13 @@ export default function Topbar() {
             <span>christianmarquez41@gmail.com</span>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+            <div className="hamburger">
+                <span className="line1"></span>
+                <span className="line2"></span>
+                <span className="line3"></span>
+            </div>
+        </div>
       </div>
     </div>
   );
