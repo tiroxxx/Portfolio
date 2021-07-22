fc98bc2987e9d8a6d4d832fbcb138fe0dd88c7e6
Christian Marquez <christianmarquez41@gmail.com>
2021-07-21 20:33:18 -0400

    added typing animation for intro text

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useRef } from 'react';
 import './intro.scss';
 import { KeyboardArrowDown } from '@material-ui/icons';
+import { init } from 'ityped';
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      strings: ['Developer', 'Gamer', "Front End Developer"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -13,7 +25,9 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Christian Marquez</h1>
-          <h3>Full Stack Developer</h3>
+          <h3>
+            Your <span ref={textRef}></span>
+          </h3>
         </div>
         <a href="#about">
           <KeyboardArrowDown className="downArrow" />
