4b87bc76201833515d7f6837ad9e8d4866894b81
Christian Marquez <christianmarquez41@gmail.com>
2021-07-21 20:18:10 -0400

    added intro message and picture

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,24 @@
-import "./intro.scss"
+import './intro.scss';
+import { KeyboardArrowDown } from '@material-ui/icons';
 
 export default function Intro() {
-    return (
-        <div className="intro" id="intro">
-            INTRO
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/Portfolio-Picture.jpg" alt="Christian Marquez" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Christian Marquez</h1>
+          <h3>Full Stack Developer</h3>
+        </div>
+        <a href="#about">
+          <KeyboardArrowDown className="downArrow" />
+        </a>
+      </div>
+    </div>
+  );
 }
