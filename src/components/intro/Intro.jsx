308014cfb24f2f78a58a19a82fe322aa131429f8
Christian Marquez <christianmarquez41@gmail.com>
2021-07-24 19:29:29 -0400

    fixing mobile version

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/Portfolio-Picture.jpg" alt="Christian Marquez" />
+          <img src="assets/selfie.png" alt="Christian Marquez" />
         </div>
       </div>
       <div className="right">
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            Christian Marquez
+            Christian M.
           </a>
           <div className="itemContainer">
             <Person className="icon" />
