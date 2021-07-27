7ed0ea8a6750f901bf091d7fef9b06cf9ad12e8b
Christian Marquez <christianmarquez41@gmail.com>
2021-07-26 23:14:42 -0400

    added workaround to IOS taskbar covering viewport issue

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import "./about.scss"
 export default function About() {
     return (
         <div className="about" id="about">
-            ABOUT
+            
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "./index.scss"
+import './index.scss';
+
+// Work around to neglect IOS taskbar when using viewport height
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 
 ReactDOM.render(
