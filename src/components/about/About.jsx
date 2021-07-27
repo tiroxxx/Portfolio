882a4ae232f78a41784957dceee61660e755a1e6
Christian Marquez <christianmarquez41@gmail.com>
2021-07-27 14:01:32 -0400

    added content to about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,27 @@
-import "./about.scss"
+import './about.scss';
 
 export default function About() {
-    return (
-        <div className="about" id="about">
-            
+  return (
+    <div className="about" id="about">
+      <div className="wrapper">
+        <div className="left">
+          <p>
+            I'm a Full Stack Developer mainly working with the MERN stack with
+            over 9 months of experience in web development. Always looking to
+            improve and optimize everything I do. Javascript is my bread and
+            MongoDB is my butter.
+          </p>
+          <p>
+            Although the front-end is beautiful and I enjoy React, the back-end
+            is where my home is.
+          </p>
+          <p>
+            When in need of guidance I go to my parents Google and Youtube, but
+            when they're mean I go to my uncle Stackoverflow.
+          </p>
         </div>
-    )
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 }
